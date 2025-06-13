a8e4fd261a30bf5048c3e7488998eae928dbbcfa
Habeeblahi Balogun <balogunhabeeb4@gmail.com>
2025-06-13 22:53:55 +0100

    Update api_config.py

--- api_config.py
+++ api_config.py
@@ -1,5 +1,5 @@
 
 import googlemaps
 
-GOOGLE_API_KEY = 'AIzaSyBEczEbQM5LxSMj7YnE_AilWIecNwzLhJk'  # Replace with your actual API key
+GOOGLE_API_KEY = ''  # Replace with your actual API key
 gmaps = googlemaps.Client(key=GOOGLE_API_KEY)
